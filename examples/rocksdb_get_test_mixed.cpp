5104015a4185cd62bae290e3aa65ffafcd71531c
Fredrik Björkman <pfl.bjorkman@gmail.com>
2022-05-26 13:08:38 +0200

    Pushing mixed to run iwht stats

--- examples/rocksdb_get_test_mixed.cpp
+++ examples/rocksdb_get_test_mixed.cpp
@@ -55,12 +55,35 @@ bool keyIsCommon(double prob){
   return uniform_zero_to_one(randomEngine) <= prob;
 }
 
+void shuffleKeys(vector<string> &keys){
+  auto rng = default_random_engine {};
+  shuffle(begin(keys), end(keys), rng);
+}
+
+vector<string> selectCommonKeys(vector<string> &commonKeys, int numKeys){
+  shuffleKeys(commonKeys);
+  auto first = commonKeys.begin();
+  auto last = commonKeys.begin() + numKeys;
+  vector<string> selectedCommonKeys(first, last);
+  return selectedCommonKeys;
+}
+
 vector<rocksdb::Slice> generateKeySlices(const vector<string>& keys){
   vector<rocksdb::Slice> keySlices;
   for(const string& key : keys){
     keySlices.emplace_back(key);
   }
   return keySlices;
+}
+
+vector<string> generateRandomTestKeys(int numOfKeysInSet, int totkeys){
+  unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
+  default_random_engine randomEngine(seed);
+  vector<string> testKeys;
+  for(int i = 0; i < numOfKeysInSet; i++){
+    testKeys.emplace_back(getKey(randomEngine()%totkeys));
+  }
+  return testKeys;
 }
 
 vector<string> generateTestKeys(int amount, vector<string> &commonKeys, int totalKeys, double prob){
@@ -75,13 +98,37 @@ vector<string> generateTestKeys(int amount, vector<string> &commonKeys, int tota
   return testKeys;
 }
 
-vector<string> generateRandomTestKeys(int numOfKeysInSet, int totkeys){
-  unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
-  default_random_engine randomEngine(seed);
+vector<string> generateTestKeysNoMultiples(int amount, vector<string> &commonKeys, int totalKeys, double prob){
+  vector<string> getKeys;
+  vector<string> mulKeys;
   vector<string> testKeys;
-  for(int i = 0; i < numOfKeysInSet; i++){
-    testKeys.emplace_back(getKey(randomEngine()%totkeys));
+  int commonKeyCount = 0;
+  int randomKeyCount = 0;
+  for(int i = 0; i < amount; i++){
+    if(keyIsCommon(prob)){
+      commonKeyCount++;
+    } else{
+      randomKeyCount++;
+    }
   }
+  vector<string> randomKeysSelection = generateRandomTestKeys(randomKeyCount*2, totalKeys);
+  vector<string> commonKeysSelection = selectCommonKeys(commonKeys, commonKeyCount*2);
+
+  getKeys.reserve(randomKeyCount + commonKeyCount);
+  getKeys.insert(getKeys.end(), randomKeysSelection.begin(), randomKeysSelection.begin() + randomKeyCount);
+  getKeys.insert(getKeys.end(), commonKeysSelection.begin(), commonKeysSelection.begin() + commonKeyCount);
+
+  mulKeys.reserve(randomKeyCount + commonKeyCount);
+  mulKeys.insert(mulKeys.end(), randomKeysSelection.begin() + randomKeyCount, randomKeysSelection.end());
+  mulKeys.insert(mulKeys.end(), commonKeysSelection.begin() + commonKeyCount, commonKeysSelection.end());
+
+  shuffleKeys(getKeys);
+  shuffleKeys(mulKeys);
+
+  testKeys.reserve(getKeys.size() + mulKeys.size());
+  testKeys.insert(testKeys.end(), getKeys.begin(), getKeys.end());
+  testKeys.insert(testKeys.end(), mulKeys.begin(), mulKeys.end());
+
   return testKeys;
 }
 
@@ -132,6 +179,7 @@ int main(int argc, char** argv) {
   rocksdb::DB* db;
   rocksdb::Options options;
   options.create_if_missing = true;
+  options.statistics = rocksdb::CreateDBStatistics();
 
   rocksdb::Status status =
       rocksdb::DB::Open(options, "/home/fredrik/testdb", &db);
@@ -157,15 +205,21 @@ int main(int argc, char** argv) {
     values.resize(numFetches);
     statuses.resize(numFetches);
     vector<string> commonKeys = generateRandomTestKeys(numFetches*10, TOTAL_KEYS);
-    // Warmup
+//     Warmup
     cout << "Starting warmup" << endl;
     doWarmup(db, value, status, values, statuses, COMMON_KEY_PROB,
              TOTAL_KEYS, commonKeys, numFetches);
     cout << "Warmup finished" << endl;
 
+    outfile.open("1000keyStats.txt", std::ios_base::app);
+    outfile << "After Warmup" << endl;
+    outfile << options.statistics->ToString() << endl;
+
     for (int i = 0; i < NUM_OF_RUNS; i++) {
-      vector<string> getKeys = generateTestKeys(numFetches, commonKeys, TOTAL_KEYS, COMMON_KEY_PROB);
-      vector<string> mulKeys = generateTestKeys(numFetches, commonKeys, TOTAL_KEYS, COMMON_KEY_PROB);
+      vector<string> testKeys = generateTestKeysNoMultiples(numFetches, commonKeys, TOTAL_KEYS, COMMON_KEY_PROB);
+      vector<string> getKeys(testKeys.begin(), testKeys.begin() + testKeys.size()/2);
+      vector<string> mulKeys(testKeys.begin() + testKeys.size()/2, testKeys.end());
+
       vector<rocksdb::Slice> getKeySlices = generateKeySlices(getKeys);
       vector<rocksdb::Slice> mulKeySlices = generateKeySlices(mulKeys);
       double getTime = get(db, getKeySlices, value, status);
@@ -182,34 +236,38 @@ int main(int argc, char** argv) {
     cout << "Avg mul: " + to_string(totMulTime/NUM_OF_RUNS) << endl;
     cout << endl;
   }
+  cout << options.statistics->ToString() << endl;
+//  outfile << "After Run" << endl;
+//  outfile << options.statistics->ToString() << endl;
+//  outfile.close();
 
-  outfile.open("get_rawdata_mixed.txt");
-  if(outfile.is_open()) {
-    for(const vector<double>& getRow : getRawData){
-      for(double rawGetData : getRow){
-        outfile << rawGetData << " ";
-      }
-      outfile << endl;
-    }
-    outfile.close();
-  }
-  else{
-    cout << "Unable to open file" << endl;
-  }
-
-  outfile.open("multiget_rawdata_mixed.txt");
-  if(outfile.is_open()) {
-    for(const vector<double>& multiGetRow : multiGetRawData){
-      for(double rawMultiGetData : multiGetRow){
-        outfile << rawMultiGetData << " ";
-      }
-      outfile << endl;
-    }
-    outfile.close();
-  }
-  else{
-    cout << "Unable to open file" << endl;
-  }
+//  outfile.open("raw_data/get_rawdata_common_v2.txt");
+//  if(outfile.is_open()) {
+//    for(const vector<double>& getRow : getRawData){
+//      for(double rawGetData : getRow){
+//        outfile << rawGetData << " ";
+//      }
+//      outfile << endl;
+//    }
+//    outfile.close();
+//  }
+//  else{
+//    cout << "Unable to open file" << endl;
+//  }
+//
+//  outfile.open("raw_data/multiget_rawdata_common_v2.txt");
+//  if(outfile.is_open()) {
+//    for(const vector<double>& multiGetRow : multiGetRawData){
+//      for(double rawMultiGetData : multiGetRow){
+//        outfile << rawMultiGetData << " ";
+//      }
+//      outfile << endl;
+//    }
+//    outfile.close();
+//  }
+//  else{
+//    cout << "Unable to open file" << endl;
+//  }
 
   delete db;
 
